eb8c7129cd45c7c9562e10027a1a2964c74f8cc6
宇阳 <3311118881@qq.com>
2024-07-25 20:22:04 +0800

    大改动

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,9 +1,15 @@
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import { Button } from 'antd';
 
 const Create = () => {
   return (
     <>
       <Breadcrumb pageName="Calendar" />
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
     </>
   );
 };
