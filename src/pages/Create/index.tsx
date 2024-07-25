255542ea6817c24a74667040e3af5adf79222863
宇阳 <3311118881@qq.com>
2024-07-25 18:49:59 +0800

    项目初始化

---
+++ src/pages/Create/index.tsx
@@ -0,0 +1,13 @@
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+
+const Create = () => {
+  return (
+    <>
+      <Breadcrumb pageName="Calendar" />
+
+      
+    </>
+  );
+};
+
+export default Create;
