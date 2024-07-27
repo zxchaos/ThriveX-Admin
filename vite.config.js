c060f261212b5e86eb8a13feee7144ee45e6aa0c
宇阳 <3311118881@qq.com>
2024-07-27 15:58:41 +0800

    配置scss样式自动引入

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite'
+import sassDts from 'vite-plugin-sass-dts';
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sassDts()],
+  // 配置快捷路径
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '~': path.resolve(__dirname, 'src')
     },
+  },
+  // 配置scss样式自动引入
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/var.scss";`
+      }
+    }
   }
 })
