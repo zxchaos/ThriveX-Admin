6f4b5a1f956311670d2440260f0b1d2b2b45f6fa
宇阳 <3311118881@qq.com>
2024-08-13 21:04:23 +0800

    评论列表默认展开

--- src/components/Header/DropdownMessage.tsx
+++ src/components/Header/DropdownMessage.tsx
@@ -2,11 +2,6 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ClickOutside from '../ClickOutside';
 
-import UserOne from '../../images/user/user-01.png';
-import UserTwo from '../../images/user/user-02.png';
-import UserThree from '../../images/user/user-03.png';
-import UserFour from '../../images/user/user-04.png';
-
 import { getCommentListAPI } from '@/api/Comment'
 import { Comment } from '@/types/comment';
 
