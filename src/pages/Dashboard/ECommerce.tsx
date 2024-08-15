70222ca7c020793df664f11426dbe5dcf8775ee8
宇阳 <3311118881@qq.com>
2024-08-15 15:27:33 +0800

    改动

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ChartOne from '../../components/Charts/ChartOne';
+import VisitorsStatisChat from './components/VisitorsStatisChat';
 import ChartThree from '../../components/Charts/ChartThree';
 import ChartTwo from '../../components/Charts/ChartTwo';
 import ChatCard from '../../components/Chat/ChatCard';
@@ -30,7 +30,7 @@ const ECommerce: React.FC = () => {
       </Card>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
+        <VisitorsStatisChat />
         <ChartTwo />
         <ChartThree />
         <MapOne />
