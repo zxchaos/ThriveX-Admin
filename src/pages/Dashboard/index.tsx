6690118146fc60fb4f1124f97d42b5302ad1cbab
宇阳 <3311118881@qq.com>
2024-08-16 12:20:30 +0800

    从github获取最近10次迭代记录

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,26 @@
 import { Card } from 'antd';
+import { useEffect, useState } from 'react';
 import HeaderInfo from './components/HeaderInfo';
 
 const Home = () => {
+  const [iterativeRecording, setIterativeRecording] = useState<string[]>([])
+
+  // 从github获取最近10次迭代记录
+  const getCommitData = async () => {
+    const res = await fetch(`https://api.github.com/repos/LiuYuYang01/ThriveX-Admin/commits?per_page=10`)
+    const data = await res.json()
+    const result = data.map((item: { commit: { message: string } }) => item.commit.message)
+    sessionStorage.setItem('iterative_recording', JSON.stringify(result))
+  }
+
+  useEffect(() => {
+    // 如果缓存中有值就无需重新调接口
+    const data = JSON.parse(sessionStorage.getItem('iterative_recording') || '[]')
+    console.log(data);
+
+    data.length ? setIterativeRecording(data) : getCommitData()
+  }, [])
+
   return (
     <>
       <Card>
