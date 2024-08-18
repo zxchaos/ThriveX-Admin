4a8403c24ea976cd832cb1039cc9c01322e19457
宇阳 <3311118881@qq.com>
2024-08-18 16:48:09 +0800

    路由列表

--- src/pages/Role/index.tsx
+++ src/pages/Role/index.tsx
@@ -1,40 +1,55 @@
 import { useState, useEffect } from 'react';
-import { Table, Button, Form, Input, Popconfirm, message, Card } from 'antd';
+import { Table, Button, Form, Input, Popconfirm, message, Card, Modal, Transfer, TransferProps } from 'antd';
+import { getRouteListAPI } from '@/api/Route';
 import { getRoleListAPI, addRoleDataAPI, editRoleDataAPI, delRoleDataAPI } from '@/api/Role';
 import { Role } from '@/types/app/role';
 import Title from '@/components/Title';
 import { ColumnsType } from 'antd/es/table';
+import "./index.scss"
 
 const RolePage = () => {
     const [loading, setLoading] = useState<boolean>(false);
     const [role, setRole] = useState<Role>({} as Role);
-    const [list, setList] = useState<Role[]>([]);
+    const [roleList, setRoleList] = useState<Role[]>([]);
+    const [routeList, setRouteList] = useState<{ key: number, title: string }[]>([]);
+
+    // 角色权限框
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const columns: ColumnsType<Role> = [
         { title: 'ID', dataIndex: 'id', key: 'id', align: 'center' },
-        { title: '角色名称', dataIndex: 'name', key: 'name' },
+        { title: '角色标识', dataIndex: 'name', key: 'name' },
         { title: '角色描述', dataIndex: 'description', key: 'description' },
         {
             title: '操作', key: 'action',
             render: (text: string, record: Role) => (
                 <>
-                    <Button onClick={() => editRoleData(record)}>修改</Button>
+                    <Button type="primary" onClick={() => setIsModalOpen(true)}>权限</Button>
+                    <Button onClick={() => editRoleData(record)} className="mx-2">修改</Button>
                     <Popconfirm title="警告" description="你确定要删除吗" okText="确定" cancelText="取消" onConfirm={() => delRoleData(record.id!)}>
-                        <Button type="primary" danger className="ml-2">删除</Button>
+                        <Button type="primary" danger>删除</Button>
                     </Popconfirm>
                 </>
             )
         }
     ];
 
+    const getRouteList = async () => {
+        setLoading(true);
+        const { data } = await getRouteListAPI();
+        setRouteList(data.map(item => ({ key: item.id, title: item.path })) as { key: number, title: string }[]);
+        setLoading(false);
+    };
+
     const getRoleList = async () => {
         setLoading(true);
         const { data } = await getRoleListAPI();
-        setList(data as Role[]);
+        setRoleList(data as Role[]);
         setLoading(false);
     };
 
     useEffect(() => {
+        getRouteList()
         getRoleList();
     }, []);
 
@@ -69,6 +84,14 @@ const RolePage = () => {
         });
     };
 
+
+    const [targetKeys, setTargetKeys] = useState<TransferProps['targetKeys']>([]);
+
+    const onChange: TransferProps['onChange'] = (nextTargetKeys) => {
+        console.log('targetKeys:', nextTargetKeys);
+        setTargetKeys(nextTargetKeys);
+    };
+
     return (
         <>
             <Title value="角色管理" />
@@ -83,8 +106,8 @@ const RolePage = () => {
                         size='large'
 
                     >
-                        <Form.Item label="角色名称" name="name" rules={[{ required: true, message: '角色名称不能为空' }]}>
-                            <Input placeholder="请输入角色名称" />
+                        <Form.Item label="角色标识" name="name" rules={[{ required: true, message: '角色标识不能为空' }]}>
+                            <Input placeholder="请输入角色标识" />
                         </Form.Item>
 
                         <Form.Item label="描述" name="description" rules={[{ required: true, message: '角色描述不能为空' }]}>
@@ -100,7 +123,7 @@ const RolePage = () => {
                 <Card className="w-[59%] [&>.ant-card-body]:!p-0">
                     <Table
                         rowKey="id"
-                        dataSource={list}
+                        dataSource={roleList}
                         columns={columns}
                         scroll={{ x: 'max-content' }}
                         pagination={{
@@ -111,6 +134,21 @@ const RolePage = () => {
                     />
                 </Card>
             </div>
+
+            <Modal title="角色权限" open={isModalOpen} onCancel={() => setIsModalOpen(false)} footer={null} className='RolePageModal'>
+                <div className='flex justify-center py-6'>
+                    <Transfer
+                        dataSource={routeList}
+                        targetKeys={targetKeys}
+                        titles={['权限列表', '当前权限']}
+                        render={(item) => item.title}
+                        onChange={onChange}
+                        showSelectAll={false}
+                    />
+                </div>
+
+                <Button type='primary' className='w-full mt-2'>保存</Button>
+            </Modal>
         </>
     );
 };
